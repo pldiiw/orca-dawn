634173dfb344920d9e52d78ad644fc5a8aa9f02b
pld <pl@pld.tf>
2025-09-23 02:20:03 +0800

    Fix Linux dependencies

--- build.py
+++ build.py
@@ -129,6 +129,8 @@ target_sources(webgpu PRIVATE ${WEBGPU_DAWN_NATIVE_PROC_GEN})"""
             "-D", "DAWN_ENABLE_DESKTOP_GL=OFF",
             "-D", "DAWN_ENABLE_OPENGLES=OFF",
             "-D", "DAWN_ENABLE_VULKAN=ON"
+            "-D", "DAWN_USE_WAYLAND=ON"
+            "-D", "DAWN_USE_X11=ON"
         ]
     else:
         assert False, "Unsupported OS"
